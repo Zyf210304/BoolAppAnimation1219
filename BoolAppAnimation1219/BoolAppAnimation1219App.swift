a7efebd6575e30aeec65f6d5fe4b4fdbd9a8d4c1
张亚飞 <672233580@qq.com>
2022-12-19 17:59:27 +0800

    Initial Commit

---
+++ BoolAppAnimation1219/BoolAppAnimation1219App.swift
@@ -0,0 +1,17 @@
+//
+//  BoolAppAnimation1219App.swift
+//  BoolAppAnimation1219
+//
+//  Created by 张亚飞 on 2022/12/19.
+//
+
+import SwiftUI
+
+@main
+struct BoolAppAnimation1219App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
